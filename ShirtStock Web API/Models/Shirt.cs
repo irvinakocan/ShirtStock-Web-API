cc7341594db1953bdb5e475c23974e45f92e8ab6
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2024-04-02 20:49:43 +0200

    Shirt model implementation

--- ShirtStock Web API/Models/Shirt.cs
+++ ShirtStock Web API/Models/Shirt.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace ShirtStock_Web_API.Models
 {
 	public class Shirt
 	{
-		public Shirt()
-		{
-		}
+		public int ShirtId { get; set; }
+
+		[Required]
+		public string Brand { get; set; }
+
+		[Required]
+		public string Color { get; set; }
+
+		public int? Size { get; set; }
+
+		[Required]
+		public string Gender { get; set; }
+
+		public double? Price { get; set; }
 	}
 }
 
